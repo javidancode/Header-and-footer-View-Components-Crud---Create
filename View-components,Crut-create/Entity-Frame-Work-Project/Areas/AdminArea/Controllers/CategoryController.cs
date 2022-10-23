d097d216d7f39acb9021f14aa414f89ddb151ad4
javidancode <javidanig@code.edu.az>
2022-10-23 06:28:12 +0400

    Added Header and footer View Components, Crud - Create

---
+++ View-components,Crut-create/Entity-Frame-Work-Project/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using Entity_Frame_Work_Project.Data;
+using Entity_Frame_Work_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entity_Frame_Work_Project.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Category> categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            bool isExist = await _context.Categories.AnyAsync(m => m.Name.Trim().ToUpper() == category.Name.Trim().ToUpper());
+
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "Category already exist");
+                return View();
+            }
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
